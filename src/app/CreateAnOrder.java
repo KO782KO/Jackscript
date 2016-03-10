24c9fca882586d326fa1ec5d0c71b9c4cab887a4
KO782KO <komalley782@gmail.com>
2016-03-10 16:31:20 -0500

    testing

--- src/app/CreateAnOrder.java
+++ src/app/CreateAnOrder.java
@@ -201,7 +201,7 @@ public class CreateAnOrder extends javax.swing.JFrame {
                             setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
                             setTitle("Create Order");
 
-                            OrderSummary.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+                            OrderSummary.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
 
                             PriceField.setEditable(false);
                             PriceField.addActionListener(new java.awt.event.ActionListener() {
