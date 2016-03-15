7d083eef5f1052eb1c05be7b876fa338a0adb705
realhexdec16 <jackn1000@gmail.com>
2016-03-15 13:31:50 -0400

    fixed items class

--- src/app/Items.java
+++ src/app/Items.java
@@ -114,18 +114,18 @@ public void sellAnItem(boolean shippingCharged, Item item, Customer customer, in
     }
 }
 
-    public double getOrderPrice(Customer customer, Item item, int units){
+    public double getOrderPrice(Transaction transaction, Item item, int units){
 
             double finalCost;
             double pricePerItem=item.getSellingPrice();
             double preCost=units*pricePerItem;
-            if(customer instanceof CreditTransaction){
-                CreditTransaction cust=(CreditTransaction) customer;
+            if(transaction instanceof CreditTransaction){
+                CreditTransaction cust=(CreditTransaction) transaction;
                 
                 finalCost=preCost+(preCost*cust.getCREDIT_MARKUP());
             }
-            else if(customer instanceof CashTransaction){
-                CashTransaction custCash=(CashTransaction) customer;
+            else if(transaction instanceof CashTransaction){
+                CashTransaction custCash=(CashTransaction) transaction;
                 finalCost=preCost-(preCost*CashTransaction.getCASH_DISCOUNT());
                 
             }
